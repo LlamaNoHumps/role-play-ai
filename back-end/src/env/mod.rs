2bf2dfaa94984ad10dd9746c1fef7d21eabe545e
L-ING <hlf01@icloud.com>
2025-09-27 10:09:39 +0800

    chore: docker构建，compose部署

--- back-end/src/env/mod.rs
+++ back-end/src/env/mod.rs
@@ -15,6 +15,8 @@ pub struct Env {
     pub mysql_username: String,
     pub mysql_password: String,
     pub mysql_endpoint: String,
+    pub qiniu_llm_model: String,
+    pub qiniu_llm_thinking_model: String,
 }
 
 impl Env {
@@ -30,6 +32,12 @@ impl Env {
         let mysql_username = get_env_value("MYSQL_USERNAME").expect("MYSQL_USERNAME must be set");
         let mysql_password = get_env_value("MYSQL_PASSWORD").expect("MYSQL_PASSWORD must be set");
         let mysql_endpoint = get_env_value("MYSQL_ENDPOINT").expect("MYSQL_ENDPOINT must be set");
+        let qiniu_llm_model = get_env_value_option(
+            "QINIU_LLM_MODEL",
+            "deepseek/deepseek-v3.1-terminus".to_string(),
+        );
+        let qiniu_llm_thinking_model =
+            get_env_value_option("QINIU_LLM_THINKING_MODEL", "deepseek-r1-0528".to_string());
 
         Self {
             port,
@@ -40,6 +48,8 @@ impl Env {
             mysql_username,
             mysql_password,
             mysql_endpoint,
+            qiniu_llm_model,
+            qiniu_llm_thinking_model,
         }
     }
 }
